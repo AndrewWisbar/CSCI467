a7d5648d06660e1b07fef4880087ea98c86066aa
Andrew Wisbar <andrewwisbar@gmail.com>
2021-11-10 16:21:13 -0600

    updated SQL script to create tables

--- SQL/Create_DatabaseV1.sql
+++ SQL/Create_DatabaseV1.sql
@@ -5,11 +5,17 @@ CREATE TABLE InventoryDatabase(
 	CONSTRAINT InventoryDatabase_PK PRIMARY KEY (ItemID)
 );	
 
-CREATE TABLE OrderDatabase(
+CREATE TABLE OrderParts(
     	OrderID	        INT			    	AUTO_INCREMENT UNIQUE NOT NULL,
     	ItemID		INT			    	NOT NULL,
+    	OrderQuantity 	INT		        	NOT NULL,
+	CONSTRAINT PRIMARY KEY(OrderID, ItemID)
+);	
+
+CREATE TABLE OrderTotals(
+    	OrderID	        INT			    	AUTO_INCREMENT UNIQUE NOT NULL,
     	TotalPrice      DECIMAL(8,2)			NOT NULL,
     	TotalWeight     DECIMAL(4,2)		    	NOT NULL,
     	OrderQuantity 	INT		        	NOT NULL,
-	CONSTRAINT PRIMARY KEY(OrderID, ItemID)
+	CONSTRAINT PRIMARY KEY(OrderID)
 );	
